fdbe05c7b7b69904ac79235690b308994ed0529e
Andrey <mendozer@yandex.ru>
2018-04-20 19:12:47 +0300

    4_3  11:50 min

--- tests_contact/test_add_contact.py
+++ tests_contact/test_add_contact.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 from model.contact import Contact
-from sys import maxsize
 
 
 def test_add_contact(app):
@@ -10,17 +9,14 @@ def test_add_contact(app):
     new_contacts = app.contact.get_contact_list()
     assert len(old_contacts) + 1 == len(new_contacts)
     old_contacts.append(contact)
-
-    def id_or_max(gr):
-        if gr.id:
-            return int(gr.id)
-        else:
-            return maxsize
-    assert sorted(old_contacts, key=id_or_max) == sorted(new_contacts, key=id_or_max)
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
 
 
-#def test_add_empty_contact(app):
-#    old_contacts = app.contact.get_contact_list()
-#    app.contact.create(Contact(firstname="", secondname="", number=""))
-#    new_contacts = app.contact.get_contact_list()
-#    assert len(old_contacts) + 1 == len(new_contacts)
+def test_add_empty_contact(app):
+    old_contacts = app.contact.get_contact_list()
+    contact = Contact(firstname="", secondname="", number="")
+    app.contact.create(contact)
+    new_contacts = app.contact.get_contact_list()
+    assert len(old_contacts) + 1 == len(new_contacts)
+    old_contacts.append(contact)
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
