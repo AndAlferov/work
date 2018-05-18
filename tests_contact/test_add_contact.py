a12978113b9f66913dd73a4a74c9ea4006a4ef1a
Алферов Андрей <a.alferov@orglot.office>
2018-05-18 18:59:21 +0300

    5_5

--- tests_contact/test_add_contact.py
+++ tests_contact/test_add_contact.py
@@ -1,12 +1,26 @@
 # -*- coding: utf-8 -*-
 from model.contact import Contact
+import pytest
+import random
+import string
 
 
-def test_add_contact(app):
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+
+testdata = [Contact(lastname="", firstname="", homephone="", workphone="", mobilephone="", secondaryphone="")] + [
+    Contact(lastname=random_string("lastname", 10), firstname=random_string("firstname", 10), homephone=random_string("homephone", 10), workphone=random_string("workphone", 10),
+            mobilephone=random_string("mobilephone", 10), secondaryphone=random_string("secondaryphone", 10))
+    for i in range(5)
+]
+
+
+@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
+def test_add_contact(app, contact):
+    pass
     old_contacts = app.contact.get_contact_list()
-    contact = Contact(lastname="last_name832540", firstname="first_name879095", homephone="home_phone674393",
-                      workphone="work_phone316353", mobilephone="mobile_phone22808",
-                      secondaryphone="phone_secondary913258")
     app.contact.create(contact)
     assert len(old_contacts) + 1 == app.contact.count()
     new_contacts = app.contact.get_contact_list()
